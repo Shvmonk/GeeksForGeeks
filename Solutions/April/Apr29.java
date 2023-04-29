fdaa3c38f0d898d6938d22719858248ac3d810c1
Shvmonk <127006739+Shvmonk@users.noreply.github.com>
2023-04-29 07:26:21 +0530

    `💡 POTD:` April 29

---
+++ Solutions/April/Apr29.java
@@ -0,0 +1,22 @@
+//FIND NUMBER - APRIL 29
+class Solution
+{
+    public long findNumber(long N)
+    {
+        int a[]={1,3,5,7,9};
+		String s="";
+		long pre=1;
+		long mul=5;
+		int times=2;
+		while(N>0){
+			long temp=(N % mul)-1;
+			long ind=(long)((temp+mul)%mul);
+			int index=(int)(ind/pre);
+			s=a[index]+s;
+			N-=mul;
+			mul=(long)Math.pow(5,times++);
+			pre*=5;
+		}
+		return Long.parseLong(s);
+    }
+}
