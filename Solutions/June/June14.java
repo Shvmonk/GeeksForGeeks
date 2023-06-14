f59647755fcf0441686bc787cdda3631113da8c8
Shvmonk <127006739+Shvmonk@users.noreply.github.com>
2023-06-14 23:13:08 +0530

    `💡 POTD:` June 14

---
+++ Solutions/June/June14.java
@@ -0,0 +1,15 @@
+//MAXIMUM DIAMONDS - JUNE 14
+class Solution {
+    static long maxDiamonds(int[] A, int N, int K) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:A){
+            pq.add(i);
+        }
+        long ans=0;
+        while(!pq.isEmpty() && K-->0){
+            ans+=pq.peek();
+            pq.add(pq.poll()/2);
+        }       
+        return ans;
+    }
+}
