f5c753467622abd200e798a850692e5d99eb3230
Shvmonk <127006739+Shvmonk@users.noreply.github.com>
2023-06-21 19:21:06 +0530

    `💡 POTD:` June 21

---
+++ Solutions/June/June21.java
@@ -0,0 +1,8 @@
+//REVERSE CODING - JUNE 21
+class Solution {
+    static int sumOfNaturals(int n) {
+        int mod = 1000000007;
+        long ans= ((long)n*(n+1))/2;
+        return (int)(ans%mod);
+    }
+};
